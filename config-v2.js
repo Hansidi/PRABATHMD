bec8ac4b58e801340870bf4b29e3977b012a0c1a
Hansidi <hansidiluni66@gmail.com>
2024-11-08 22:14:52 +0300

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "ghp_ava5RQuQqsVmk7E7IRo40mgwssto3l42iBc"
+const BOT_NUMBER = "94701242477"
+const SESSION_ID = "PRABATH-MD~GThiCZDb#THPeobzeXtTlWoYURKLNeIMBT0_mO4ahJ20sCGalfVk"
 
 
 module.exports = {
